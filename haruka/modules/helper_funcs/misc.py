daeec786d98e2dce45f71e9928b09f523cb14f43
mama-userbot <77100101+mama-userbot@users.noreply.github.com>
2021-03-12 00:00:31 +0530

    Update misc.py

--- haruka/modules/helper_funcs/misc.py
+++ haruka/modules/helper_funcs/misc.py
@@ -10,6 +10,7 @@ from telegram.ext import CommandHandler, Filters, MessageHandler, CallbackQueryH
 
 
 class EqInlineKeyboardButton(InlineKeyboardButton):
+
     def __eq__(self, other):
         return self.text == other.text
 
@@ -41,46 +42,53 @@ def split_message(msg: str) -> List[str]:
         return result
 
 
-def paginate_modules(chat_id, page_n: int, module_dict: Dict, prefix, chat=None) -> List:
+def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if not chat:
-        modules = sorted(
-            [EqInlineKeyboardButton(tld(chat_id, x.__mod_name__),
-                                    callback_data="{}_module({})".format(prefix, x.__mod_name__.lower())) for x
-             in module_dict.values()])
+        modules = sorted([
+            EqInlineKeyboardButton(
+                x.__mod_name__,
+                callback_data="{}_module({})".format(prefix,
+                                                     x.__mod_name__.lower()))
+            for x in module_dict.values()
+        ])
     else:
-        modules = sorted(
-            [EqInlineKeyboardButton(tld(chat_id, x.__mod_name__),
-                                    callback_data="{}_module({},{})".format(prefix, chat, x.__mod_name__.lower())) for x
-             in module_dict.values()])
+        modules = sorted([
+            EqInlineKeyboardButton(
+                x.__mod_name__,
+                callback_data="{}_module({},{})".format(prefix, chat,
+                                                        x.__mod_name__.lower()))
+            for x in module_dict.values()
+        ])
 
-    pairs = list(zip(modules[::2], modules[1::2]))
+    pairs = [
+        modules[i * 3:(i + 1) * 3] for i in range((len(modules) + 3 - 1) // 3)
+    ]
 
-    if len(modules) % 2 == 1:
+    round_num = len(modules) / 3
+    calc = len(modules) - round(round_num)
+    if calc == 1:
         pairs.append((modules[-1],))
-
-    max_num_pages = ceil(len(pairs) / 7)
-    modulo_page = page_n % max_num_pages
-
-    # can only have a certain amount of buttons side by side
-    if len(pairs) > 7:
-        pairs = pairs[modulo_page * 7:7 * (modulo_page + 1)] + [
-            (EqInlineKeyboardButton("<<", callback_data="{}_prev({})".format(prefix, modulo_page)),
-             EqInlineKeyboardButton("⬅️ Back", callback_data="bot_start"),
-             EqInlineKeyboardButton(">>", callback_data="{}_next({})".format(prefix, modulo_page)))]
+    elif calc == 2:
+        pairs.append((modules[-1],))
+        
     else:
-        pairs += [[EqInlineKeyboardButton("⬅️ Back", callback_data="bot_start")]]
-
+        pairs += [[EqInlineKeyboardButton("Back to Info",  callback_data="aboutmanu_")]]
 
     return pairs
 
 
-def send_to_list(bot: Bot, send_to: list, message: str, markdown=False, html=False) -> None:
+def send_to_list(bot: Bot,
+                 send_to: list,
+                 message: str,
+                 markdown=False,
+                 html=False) -> None:
     if html and markdown:
         raise Exception("Can only send with either markdown or HTML!")
     for user_id in set(send_to):
         try:
             if markdown:
-                bot.send_message(user_id, message, parse_mode=ParseMode.MARKDOWN)
+                bot.send_message(
+                    user_id, message, parse_mode=ParseMode.MARKDOWN)
             elif html:
                 bot.send_message(user_id, message, parse_mode=ParseMode.HTML)
             else:
@@ -111,16 +119,18 @@ def revert_buttons(buttons):
     return res
 
 
-def is_module_loaded(name):
-    return (not LOAD or name in LOAD) and name not in NO_LOAD
-
-
-def user_bot_owner(func):
-    @wraps(func)
-    def is_user_bot_owner(bot: Bot, update: Update, *args, **kwargs):
-        user = update.effective_user
-        if user and user.id == OWNER_ID:
-            return func(bot, update, *args, **kwargs)
+def build_keyboard_parser(bot, chat_id, buttons):
+    keyb = []
+    for btn in buttons:
+        if btn.url == "{rules}":
+            btn.url = "http://t.me/{}?start={}".format(bot.username, chat_id)
+        if btn.same_line and keyb:
+            keyb[-1].append(InlineKeyboardButton(btn.name, url=btn.url))
         else:
-            pass
-    return is_user_bot_owner
+            keyb.append([InlineKeyboardButton(btn.name, url=btn.url)])
+
+    return keyb
+
+
+def is_module_loaded(name):
+    return name not in NO_LOAD
